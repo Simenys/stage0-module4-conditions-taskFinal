98746f7b7990d5e027fae6752ca6347b8faac866
Simonas Zilevicius <s.zilevicius@gmail.com>
2023-08-11 16:17:59 +0300

    Whyyyyy?

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -5,5 +5,7 @@ public class BitwiseValuesSwap {
         first = first ^ second;
         second = first ^ second;
         first = first ^ second;
+        System.out.print(first + "\n" + second);
+
     }
 }
